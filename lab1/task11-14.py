9ebcd86d570d9761f7acd45bf0668fdfc6ac02a8
unknown <shherbina0@bk.ru>
2024-02-08 18:00:46 +0300

    made task11-14 func3

--- lab1/task11-14.py
+++ lab1/task11-14.py
@@ -57,3 +57,12 @@ def sort2(l):
     
     return sorted(l, key=keyFunc)
 
+def sort3(l):
+    def keyFunc(string):
+        k = 0
+        for i in range(1,len(string)):
+            if ((string[i-1] in ["a", "e", "y", "u", "i", "o"]) and (string[i] not in ["a", "e", "y", "u", "i", "o"])) or ((string[i-1] not in ["a", "e", "y", "u", "i", "o"]) and (string[i] in ["a", "e", "y", "u", "i", "o"])):
+                k += 1
+        return k
+    return sorted(l, key=keyFunc)
+
