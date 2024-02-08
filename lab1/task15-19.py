b6c33027def28a3160626fd2750878cc8b97d1e9
unknown <shherbina0@bk.ru>
2024-02-08 19:01:27 +0300

    made task15-19 func2

--- lab1/task15-19.py
+++ lab1/task15-19.py
@@ -2,3 +2,8 @@ def func1(l, i):
     if l[i] == max(l): return True
     else: return False
 
+def func2(l, i):
+    if i != 0 and l[i-1] < l[i]: return False
+    if i != len(l) - 1 and l[i+1] < l[i]: return False
+    return True
+
