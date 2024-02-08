c484556b93ae3335cc8363c4e45657680b2a491e
unknown <shherbina0@bk.ru>
2024-02-08 19:12:40 +0300

    made task15-19 func4

--- lab1/task15-19.py
+++ lab1/task15-19.py
@@ -14,3 +14,11 @@ def func3(l):
     l[len(l)-1] = first
     return l
 
+def func4(l):
+    l1 = []
+    l2 = []
+    for i in range(0,len(l)):
+        if i % 2 == 0: l1.append(l[i])
+        else: l2.append(l[i])
+    return l1 + l2
+
