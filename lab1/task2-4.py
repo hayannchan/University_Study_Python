0409b0a0ad047a3a82b804f423f8e3cb1acc5e99
unknown <shherbina0@bk.ru>
2024-02-08 15:08:47 +0300

    made task2-4 func2

--- lab1/task2-4.py
+++ lab1/task2-4.py
@@ -4,3 +4,10 @@ def f1(string):
     l = string.split()
     random.shuffle(l)
     return " ".join(l)
+
+def f2(string):
+    k = 0
+    l = string.split()
+    for i in l:
+        if len(i) % 2 == 0: k += 1
+    return k
